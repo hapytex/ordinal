c4b50080e9642a898752d5aed131b3426b538112
Willem Van Onsem <vanonsem.willem@gmail.com>
2020-09-19 23:07:33 +0200

    reorganize languages

---
+++ src/Text/Numerals.hs
@@ -0,0 +1,9 @@
+module Text.Numerals (
+    module Text.Numerals.Algorithm
+  , module Text.Numerals.Languages
+  , module Text.Numerals.Prefix
+  ) where
+
+import Text.Numerals.Algorithm
+import Text.Numerals.Languages
+import Text.Numerals.Prefix
