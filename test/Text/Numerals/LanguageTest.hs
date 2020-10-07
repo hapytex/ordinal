44de744aaf94e9e678884bff5a23573a1f027c85
Willem Van Onsem <vanonsem.willem@gmail.com>
2020-10-07 19:45:20 +0200

    update tests

--- test/Text/Numerals/LanguageTest.hs
+++ test/Text/Numerals/LanguageTest.hs
@@ -9,7 +9,7 @@ import Data.Word(Word8, Word16, Word32, Word64)
 import Test.Hspec(SpecWith, describe, it, shouldBe)
 import Test.QuickCheck(property)
 
-import Text.Numerals.Class(toCardinal, toOrdinal)
+import Text.Numerals.Class(toCardinal, toOrdinal, toShortOrdinal)
 import Text.Numerals.Algorithm(NumeralsAlgorithm)
 
 testDifferCardinal :: NumeralsAlgorithm -> Integer -> Integer -> Bool
@@ -33,8 +33,12 @@ testEquivalenceCardinal al i = toCardinal al i == toCardinal al (fromIntegral i 
 testEquivalenceOrdinal :: Integral i => NumeralsAlgorithm -> i -> Bool
 testEquivalenceOrdinal al i = toOrdinal al i == toOrdinal al (fromIntegral i :: Integer)
 
-testLanguage :: String -> NumeralsAlgorithm -> [(Integer, Text)] -> [(Integer, Text)] -> SpecWith ()
-testLanguage languageName al cs os = describe languageName $ do
+testEquivalenceShortOrdinal :: Integral i => NumeralsAlgorithm -> i -> Bool
+testEquivalenceShortOrdinal al i = toShortOrdinal al i == toShortOrdinal al (fromIntegral i :: Integer)
+
+
+testLanguage :: String -> NumeralsAlgorithm -> [(Integer, Text)] -> [(Integer, Text)] -> [(Integer, Text)] -> SpecWith ()
+testLanguage languageName al cs os ss = describe languageName $ do
     describe "Automatied tests" $ do
         it "Different cardinal names" (property (testDifferCardinal al))
         it "Different ordinal names" (property (testDifferOrdinal al))
@@ -60,5 +64,17 @@ testLanguage languageName al cs os = describe languageName $ do
         it "Check toOrdinal algorithm with Word16" (property (testEquivalenceOrdinal al :: Word16 -> Bool))
         it "Check toOrdinal algorithm with Word32" (property (testEquivalenceOrdinal al :: Word32 -> Bool))
         it "Check toOrdinal algorithm with Word64" (property (testEquivalenceOrdinal al :: Word64 -> Bool))
+        it "Check toShortOrdinal algorithm with Int" (property (testEquivalenceShortOrdinal al :: Int -> Bool))
+        it "Check toShortOrdinal algorithm with Int8" (property (testEquivalenceShortOrdinal al :: Int8 -> Bool))
+        it "Check toShortOrdinal algorithm with Int16" (property (testEquivalenceShortOrdinal al :: Int16 -> Bool))
+        it "Check toShortOrdinal algorithm with Int32" (property (testEquivalenceShortOrdinal al :: Int32 -> Bool))
+        it "Check toShortOrdinal algorithm with Int64" (property (testEquivalenceShortOrdinal al :: Int64 -> Bool))
+        it "Check toShortOrdinal algorithm with Word" (property (testEquivalenceShortOrdinal al :: Word -> Bool))
+        it "Check toShortOrdinal algorithm with Word8" (property (testEquivalenceShortOrdinal al :: Word8 -> Bool))
+        it "Check toShortOrdinal algorithm with Word16" (property (testEquivalenceShortOrdinal al :: Word16 -> Bool))
+        it "Check toShortOrdinal algorithm with Word32" (property (testEquivalenceShortOrdinal al :: Word32 -> Bool))
+        it "Check toShortOrdinal algorithm with Word64" (property (testEquivalenceShortOrdinal al :: Word64 -> Bool))
+
     describe "Test cardinal numbers" (mapM_ (uncurry (testNumberConversion (toCardinal al))) cs)
     describe "Test ordinal numbers" (mapM_ (uncurry (testNumberConversion (toOrdinal al))) os)
+    describe "Test short ordinal numbers" (mapM_ (uncurry (testNumberConversion (toShortOrdinal al))) ss)
